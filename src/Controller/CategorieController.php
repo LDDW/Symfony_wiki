64f0aae4528d27e915084cd0174d18003fc93af2
jules <jules.boismodnd@gmail.com>
2022-11-21 22:16:20 +0100

    mirgations, catégories

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Articles;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CategorieController extends AbstractController
 {
-    #[Route('/categorie', name: 'app_categorie')]
-    public function index(): Response
+    // #[Route('/categorie', name: 'app_categorie')]
+    // public function index(): Response
+    // {
+    //     return $this->render('categorie/index.html.twig', [
+    //         'controller_name' => 'CategorieController',
+    //     ]);
+    // }
+
+    #[Route('/categorie/{id_categorie}', name: 'show_categorie')]
+    public function show_categorie(ManagerRegistry $doctrine,  int $id_categorie): Response
     {
+        $articles = $doctrine->getRepository(Articles::class)->findBy(['categorie' => $id_categorie]);
+
         return $this->render('categorie/index.html.twig', [
-            'controller_name' => 'CategorieController',
+            'articles' => $articles
         ]);
     }
 }
---
+++ src/Entity/Articles.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticlesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticlesRepository::class)]
+class Articles
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $categorie = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $auteur = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $informations = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(string $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(string $auteur): self
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getInformations(): ?string
+    {
+        return $this->informations;
+    }
+
+    public function setInformations(string $informations): self
+    {
+        $this->informations = $informations;
+
+        return $this;
+    }
+}
