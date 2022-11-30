37ab113a1e8c6e5022b58c03e4f03443a8feddcf
jules <jules.boismodnd@gmail.com>
2022-11-30 14:22:59 +0100

    Push jules

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $cat1 = random_int(1, 4);
+        $cat2 = random_int(1, 4);
+
+        $article1 = $doctrine->getRepository(Articles::class)->find($cat1);
+        $article2 = $doctrine->getRepository(Articles::class)->find($cat2);
         return $this->render('accueil/index.html.twig', [
-            'controller_name' => 'AccueilController',
+            'article1' => $article1,
+            'article2' => $article2
         ]);
     }
 }
---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Articles;
+
+class ArticleController extends AbstractController
+{
+    #[Route('/article/{id}', name: 'article_show')]
+    public function index(ManagerRegistry $doctrine,int $id): Response
+    {
+        $article = $doctrine->getRepository(Articles::class)->find($id);
+        if (!$article) {
+            throw $this->createNotFoundException(
+                "Aucun produit n'a été trouvé pour l'id" . $id
+            );
+        }
+
+        return $this->render('article/index.html.twig', [
+            'article' => $article,
+        ]);
+    }
+}
