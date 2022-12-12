9db9fa5979c76fdf90bd041c85961d33ab29fc7b
The-Cyril5555 <cyrilbizouarn5@gmail.com>
2022-12-12 14:28:15 +0100

    page mes articles

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -15,12 +15,16 @@ class AccueilController extends AbstractController
     public function index(ManagerRegistry $doctrine): Response
     {
         $categories = $doctrine->getRepository(Categorie::class)->findAll();
+        $categorie1 = $doctrine->getRepository(Categorie::class)->findBy(['id' => 1]);
+        $categorie2 = $doctrine->getRepository(Categorie::class)->findBy(['id' => 2]);
         $article1 = $doctrine->getRepository(Articles::class)->findBy(['categorie' => 1]);
         $article2 = $doctrine->getRepository(Articles::class)->findBy(['categorie' => 2]);
         return $this->render('accueil/index.html.twig', [
             'article1' => $article1[random_int(0, 3)],
             'article2' => $article2[random_int(0, 3)],
             'categories' => $categories,
+            'categorie1' => $categorie1[random_int(0, 3)],
+            'categorie2' => $categorie2[random_int(0, 3)],
         ]);
     }
 }
