91aa2d8d72fda9c3272c973c04d4a144f44ec951
jules <julesbsd56@gmail.com>
2022-12-12 14:01:35 +0100

    Push catégories aléatoires

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -18,9 +18,10 @@ class AccueilController extends AbstractController
         $article1 = $doctrine->getRepository(Articles::class)->findBy(['categorie' => 1]);
         $article2 = $doctrine->getRepository(Articles::class)->findBy(['categorie' => 2]);
         return $this->render('accueil/index.html.twig', [
-            'article1' => $article1[random_int(0, 3)],
-            'article2' => $article2[random_int(0, 3)],
-            'categories' => $categories,
+            'article1' => $article1[random_int(0, count($article1) - 1)],
+            'article2' => $article2[random_int(0, count($article2) - 1)],
+            'categorie1' => $categories[random_int(0, count($categories) - 1)],
+            'categorie2' => $categories[random_int(0, count($categories) - 1)],
         ]);
     }
 }
